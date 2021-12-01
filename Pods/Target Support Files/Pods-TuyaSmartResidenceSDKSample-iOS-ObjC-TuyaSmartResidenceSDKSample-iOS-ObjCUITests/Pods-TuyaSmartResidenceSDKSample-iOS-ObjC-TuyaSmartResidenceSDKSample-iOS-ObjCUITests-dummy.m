8985748358d9145acfb1f727c714ffb4f01defac
Vincen-Zhang <knowanswer7@gmail.com>
2021-12-01 17:50:59 +0800

    remove pod

--- Pods/Target Support Files/Pods-TuyaSmartResidenceSDKSample-iOS-ObjC-TuyaSmartResidenceSDKSample-iOS-ObjCUITests/Pods-TuyaSmartResidenceSDKSample-iOS-ObjC-TuyaSmartResidenceSDKSample-iOS-ObjCUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TuyaSmartResidenceSDKSample_iOS_ObjC_TuyaSmartResidenceSDKSample_iOS_ObjCUITests : NSObject
-@end
-@implementation PodsDummy_Pods_TuyaSmartResidenceSDKSample_iOS_ObjC_TuyaSmartResidenceSDKSample_iOS_ObjCUITests
-@end
--- Pods/Target Support Files/Pods-TuyaSmartResidenceSDKSample-iOS-ObjCTests/Pods-TuyaSmartResidenceSDKSample-iOS-ObjCTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TuyaSmartResidenceSDKSample_iOS_ObjCTests : NSObject
-@end
-@implementation PodsDummy_Pods_TuyaSmartResidenceSDKSample_iOS_ObjCTests
-@end
